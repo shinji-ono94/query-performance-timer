0d4fe39920a72819049b5baec6d039aad252d778
shinji-ono94 <1530sinnzi1003@gmail.com>
2022-07-30 13:20:38 +0900

    package slice

--- src/main/java/com/example/qpc/QPC1.java
+++ src/main/java/com/example/qpc/QPC1.java
@@ -1,4 +1,4 @@
-package com.example.qpc;
+package com.example.qpc1;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
--- src/main/java/com/example/qpc/QPC2.java
+++ src/main/java/com/example/qpc/QPC2.java
@@ -1,4 +1,4 @@
-package com.example.qpc;
+package com.example.qpc2;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
