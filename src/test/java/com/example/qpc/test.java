a201939f293797f841345e86fb939061eb2c1ffe
shinji-ono94 <1530sinnzi1003@gmail.com>
2022-08-03 09:26:32 +0900

    test reolace

--- src/test/java/com/example/qpc/test.java
+++ src/test/java/com/example/qpc/test.java
@@ -22,72 +22,139 @@ class test {
     }
 
     @Test
-    @DisplayName("Test01 : QPC1 wait test")
+    @DisplayName("Test01 : QPC1 TEST wait")
     void Test01() throws Exception {
-
-        long start = System.nanoTime();
-
-        com.example.qpc.QPC1.wait(10);
-
-        long end = System.nanoTime();
-        System.out.println((end - start));
-    }
-
-    @Test
-    @DisplayName("Test02 : QPC1 wait_noSleep test")
-    void Test02() throws Exception {
-
-        long start = System.nanoTime();
-
-        com.example.qpc.QPC1.wait_noSleep(10);
-
-        long end = System.nanoTime();
-        System.out.println((end - start));
-    }
-
-    @Test
-    @DisplayName("Test03 : QPC1 wait_nanoTime test")
-    void Test03() throws Exception {
 
         // 1ms
         for (int i = 0; i < 1000; i++) {
-            long start = System.nanoTime();
-
-            com.example.qpc.QPC1.wait_nanoTime(1);
-
-            long end = System.nanoTime();
-            System.out.println((end - start));
+            wait_qpc1_bench(1);
         }
 
         // 10ms
         for (int i = 0; i < 1000; i++) {
-            long start = System.nanoTime();
-
-            com.example.qpc.QPC1.wait_nanoTime(10);
-
-            long end = System.nanoTime();
-            System.out.println((end - start));
+            wait_qpc1_bench(10);
         }
 
         // 100ms
         for (int i = 0; i < 1000; i++) {
-            long start = System.nanoTime();
-
-            com.example.qpc.QPC1.wait_nanoTime(100);
-
-            long end = System.nanoTime();
-            System.out.println((end - start));
+            wait_qpc1_bench(100);
         }
 
         // 1000ms
         for (int i = 0; i < 1000; i++) {
-            long start = System.nanoTime();
-
-            com.example.qpc.QPC1.wait_nanoTime(1000);
-
-            long end = System.nanoTime();
-            System.out.println((end - start));
+            wait_qpc1_bench(1000);
         }
+    }
+
+    @Test
+    @DisplayName("Test02 : QPC1 TEST wait by noSleep")
+    void Test02() throws Exception {
+
+        // 1ms
+        for (int i = 0; i < 1000; i++) {
+            wait_noSleep_bench(1);
+        }
+
+        // 10ms
+        for (int i = 0; i < 1000; i++) {
+            wait_noSleep_bench(10);
+        }
+
+        // 100ms
+        for (int i = 0; i < 1000; i++) {
+            wait_noSleep_bench(100);
+        }
+
+        // 1000ms
+        for (int i = 0; i < 1000; i++) {
+            wait_noSleep_bench(1000);
+        }
+    }
+
+    @Test
+    @DisplayName("Test03 : QPC1 TEST wait by nanoTime")
+    void Test03() throws Exception {
+
+        // 1ms
+        for (int i = 0; i < 1000; i++) {
+            wait_nanoTime_bench(1);
+        }
+
+        // 10ms
+        for (int i = 0; i < 1000; i++) {
+            wait_nanoTime_bench(10);
+        }
+
+        // 100ms
+        for (int i = 0; i < 1000; i++) {
+            wait_nanoTime_bench(100);
+        }
+
+        // 1000ms
+        for (int i = 0; i < 1000; i++) {
+            wait_nanoTime_bench(1000);
+        }
+    }
+
+    @Test
+    @DisplayName("Test04 : QPC2 TEST wait")
+    void Test04() throws Exception {
+
+        // 1ms
+        for (int i = 0; i < 1000; i++) {
+            wait_qpc2_bench(1);
+        }
+
+        // 10ms
+        for (int i = 0; i < 1000; i++) {
+            wait_qpc2_bench(10);
+        }
+
+        // 100ms
+        for (int i = 0; i < 1000; i++) {
+            wait_qpc2_bench(100);
+        }
+
+        // 1000ms
+        for (int i = 0; i < 1000; i++) {
+            wait_qpc2_bench(1000);
+        }
+    }
+
+    public static void wait_qpc1_bench(int msec) {
+        long start = System.nanoTime();
+
+        com.example.qpc.QPC1.wait(msec);
+
+        long end = System.nanoTime();
+        System.out.println((end - start));
+    }
+    
+    public static void wait_noSleep_bench(int msec) {
+        long start = System.nanoTime();
+
+        com.example.qpc.QPC1.wait_noSleep(msec);
+
+        long end = System.nanoTime();
+        System.out.println((end - start));
+    }
+
+    public static void wait_nanoTime_bench(int msec) {
+        long start = System.nanoTime();
+
+        com.example.qpc.QPC1.wait_nanoTime(msec);
+
+        long end = System.nanoTime();
+        System.out.println((end - start));
+    }
+
+    public static void wait_qpc2_bench(int msec) {
+        long start = System.nanoTime();
+
+        com.example.qpc.QPC2.wait(msec);
+
+        long end = System.nanoTime();
+        System.out.println((end - start));
     }
 
     @AfterEach
